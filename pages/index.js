239818291289f137cf4b4034f9eec2559efdd99b
Mandan Kazzazi <92727403+MandanK@users.noreply.github.com>
2022-02-09 22:59:05 +0100

    Add a new video

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
             <iframe
               width="560"
               height="315"
-              src="https://www.youtube.com/embed/bgc40jgLGag"
+              src="https://www.youtube.com/embed/82oJCQWT0IA"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"

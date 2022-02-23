368a582c3ce3c3b72b59f471ad30288b9f3ec23c
Mandan Kazzazi <92727403+MandanK@users.noreply.github.com>
2022-02-23 18:36:09 +0100

    add thank you page

--- pages/thankYou.js
+++ pages/thankYou.js
@@ -1,21 +1,58 @@
+import { css } from '@emotion/react';
 import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../components/Layout';
 
+const h1Style = css`
+  font-size: 38px;
+  font-weight: bold;
+  padding-top: 95px;
+  text-align: center;
+`;
+
+const buttonStyle = css`
+  text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+  background-color: #ffcc4d;
+  border: 0.2px solid #b8b8b8;
+  margin-bottom: 80px;
+  width: 160px;
+  height: 45px;
+  margin-left: 418px;
+  margin-top: -10px;
+  cursor: pointer;
+`;
+
+const imageStyle = css`
+  text-align: center;
+  margin-left: 70px;
+  margin-top: 0px;
+`;
 export default function ThankYou() {
   return (
-    <>
-      <Head>
-        <title>Thank you for your order</title>
-        <meta
-          name="order confirmation"
-          content="Order confirmation and thank you page"
-        />
-      </Head>
-      <h1>Congrats mate! You are now officially a weirdo!</h1>
+    <Layout>
+      <>
+        <Head>
+          <title>Thank you for your order</title>
+          <meta
+            name="order confirmation"
+            content="Order confirmation and thank you page"
+          />
+        </Head>
+        <h1 css={h1Style}>Congrats mate! You are now officially a </h1>
 
-      <Link href="/" passHref>
-        <button>Back to Weirdies</button>
-      </Link>
-    </>
+        <img
+          css={imageStyle}
+          src="/images/weirdo.jpg"
+          height="350"
+          alt="the word weirdo"
+        />
+
+        <Link href="/" passHref>
+          <button css={buttonStyle}>Back to Weirdies</button>
+        </Link>
+      </>
+    </Layout>
   );
 }
